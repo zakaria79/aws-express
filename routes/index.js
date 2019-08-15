59207e0e876616cd0582dfc4444a03bc9b9b4210
Zakaria OTHMANE <othmane.zakaria79@gmail.com>
2019-08-16 00:48:22 +0200

    Test

--- routes/index.js
+++ routes/index.js
@@ -2,10 +2,8 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('*', function(req, res, next) {
-  // res.render('zakaria');
-  res.sendFile('index.html');
-  // res.render('index', {title: 'Express'});
+router.get('/*', function(req, res, next) {
+  res.render('index', {title: 'Express'});
 });
 
 router.get('/zakaria', (req, res, next) => {
