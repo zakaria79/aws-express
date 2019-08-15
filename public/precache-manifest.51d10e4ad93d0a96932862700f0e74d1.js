4b06d0aed06ac66190d2142a1008864344872148
Zakaria OTHMANE <othmane.zakaria79@gmail.com>
2019-08-16 00:26:59 +0200

    React added

---
+++ public/precache-manifest.51d10e4ad93d0a96932862700f0e74d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45ba219c2791f47a426dcbfae37ef142",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f9588274206d2aecc979",
+    "url": "/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "47a35ff2ed2ac92d266d",
+    "url": "/static/js/2.8d69a4dc.chunk.js"
+  },
+  {
+    "revision": "f9588274206d2aecc979",
+    "url": "/static/js/main.c37e9479.chunk.js"
+  },
+  {
+    "revision": "2bd07c679805b08b49cb",
+    "url": "/static/js/runtime~main.ea92a0de.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
