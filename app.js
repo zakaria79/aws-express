a0bfc07c451e50ca84d9f28a859e13e59109d2e7
Zakaria OTHMANE <othmane.zakaria79@gmail.com>
2019-08-16 00:14:10 +0200

    First commit

---
+++ app.js
@@ -0,0 +1,24 @@
+var cluster = require('cluster'),
+  app = require('./express-app');
+
+var workers = {},
+  count = require('os').cpus().length;
+
+function spawn() {
+  var worker = cluster.fork();
+  workers[worker.pid] = worker;
+  return worker;
+}
+
+if (cluster.isMaster) {
+  for (var i = 0; i < count; i++) {
+    spawn();
+  }
+  cluster.on('death', function(worker) {
+    console.log('worker ' + worker.pid + ' died. spawning a new process...');
+    delete workers[worker.pid];
+    spawn();
+  });
+} else {
+  app.listen(process.env.PORT || 5000);
+}
